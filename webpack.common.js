6690269347548285ccde6bdbc4ad760f49a09eec
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2024-12-07 19:45:56 +1000

    Reset back to main config file

--- webpack.common.js
+++ webpack.common.js
@@ -1,40 +1,40 @@
-const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const path = require('path');
+//  const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     app: './src/index.js',
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Production',
-       template: "./src/template.html",
-     }),
-   ],
-   output: {
-    //  filename: '[name].bundle.js',
-     filename: "main.js",
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
-   module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
- };
+//  module.exports = {
+//    entry: {
+//      app: './src/index.js',
+//    },
+//    plugins: [
+//      new HtmlWebpackPlugin({
+//        title: 'Production',
+//        template: "./src/template.html",
+//      }),
+//    ],
+//    output: {
+//     //  filename: '[name].bundle.js',
+//      filename: "main.js",
+//      path: path.resolve(__dirname, 'dist'),
+//      clean: true,
+//    },
+//    module: {
+//         rules: [
+//             {
+//                 test: /\.css$/i,
+//                 use: ["style-loader", "css-loader"],
+//             },
+//             {
+//                 test: /\.html$/i,
+//                 loader: "html-loader",
+//             },
+//             {
+//                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//                 type: "asset/resource",
+//             },
+//             {
+//                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
+//                 type: 'asset/resource',
+//             },
+//         ],
+//     },
+//  };
